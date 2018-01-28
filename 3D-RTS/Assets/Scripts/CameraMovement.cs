e5b6b46fe57418fdaf6e2760321bc5def76e716c
Brady Jessup <bradyjessup@WINDOWS2.SCS.CARLETON.CA>
2018-01-27 19:29:51 -0500

    Added a basic unit script for movement and a movement script for the camera control

---
+++ 3D-RTS/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public float cameraSpeed;
+    public float deadZoneOffset;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        Vector3 current = transform.position;
+        Vector3 destination = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * cameraSpeed * Time.deltaTime, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * cameraSpeed * Time.deltaTime);
+        transform.position = Vector3.Lerp(current, destination, cameraSpeed / 10.0f);
+    }
+}
