d1de1a81dd2d37fd1f25d8b076d345b8521e2c1d
Ershany <bradyjessup@hotmail.com>
2018-02-16 18:58:40 -0500

    Added smoothing, targeting movement, and mouse support for the Camera's movement functionality

--- 3D-RTS/Assets/Scripts/CameraMovement.cs
+++ 3D-RTS/Assets/Scripts/CameraMovement.cs
@@ -4,18 +4,52 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-    public float cameraSpeed;
-    public float deadZoneOffset;
+    public Transform cameraTarget;
+    public float smoothing = 0.5f;
+    public float cameraSpeed = 50.0f;
+    public float mouseDeadzoneOffset = 45.0f;
 
-	void Start ()
+    private Vector3 cameraOffset;
+
+	void Awake ()
     {
-		
-	}
-	
-	void Update ()
+        cameraOffset = new Vector3(0.0f, transform.position.y, -20.0f); // Used for camera targeting
+    }
+
+    void Update()
     {
-        Vector3 current = transform.position;
-        Vector3 destination = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * cameraSpeed * Time.deltaTime, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * cameraSpeed * Time.deltaTime);
-        transform.position = Vector3.Lerp(current, destination, cameraSpeed / 10.0f);
+        // Free movement
+        if (cameraTarget == null)
+        {
+            float horizontalMovement = Mathf.Clamp(Input.GetAxis("Horizontal") + CheckHorizontalMouse(), -1.0f, 1.0f);
+            float verticalMovement = Mathf.Clamp(Input.GetAxis("Vertical") + CheckVerticalMouse(), -1.0f, 1.0f);
+
+            Vector3 destination = new Vector3(transform.position.x + horizontalMovement * cameraSpeed * Time.deltaTime, transform.position.y, transform.position.z + verticalMovement * cameraSpeed * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, destination, cameraSpeed * Time.deltaTime);
+        }
+    }
+
+    void LateUpdate ()
+    {
+        // Targeting movement
+        if (cameraTarget)
+        {
+            Vector3 destination = cameraTarget.position + cameraOffset;
+            transform.position = Vector3.Lerp(transform.position, destination, smoothing * Time.deltaTime);
+        }
+    }
+
+    float CheckHorizontalMouse()
+    {
+        if (Input.mousePosition.x < mouseDeadzoneOffset) return -1.0f;
+        if (Input.mousePosition.x > Screen.width - mouseDeadzoneOffset) return 1.0f;
+        return 0.0f;
+    }
+
+    float CheckVerticalMouse()
+    {
+        if (Input.mousePosition.y < mouseDeadzoneOffset) return -1.0f;
+        if (Input.mousePosition.y > Screen.height - mouseDeadzoneOffset) return 1.0f;
+        return 0.0f;
     }
 }
