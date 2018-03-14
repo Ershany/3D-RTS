9d5295c1e84564171c993c5b9f9bbbe72a9014a9
Brady Jessup <bradyjessup@WINDOWS2.SCS.CARLETON.CA>
2018-03-14 18:42:26 -0400

    Adding building fixes

--- 3D-RTS/Assets/Scripts/GuildHallController.cs
+++ 3D-RTS/Assets/Scripts/GuildHallController.cs
@@ -7,11 +7,23 @@ public class GuildHallController : MonoBehaviour {
     // Unit Stats
     [Range(1.0f, 1000.0f)] public float buildingHealth = 500.0f;
 
-    // Construct the Unit
     public Building building;
+    
 
     void Awake()
     {
         building = new Building(gameObject, buildingHealth);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Enter");
+        building.OnTriggerEnter();
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exit");
+        building.OnTriggerExit();
     }
 }
