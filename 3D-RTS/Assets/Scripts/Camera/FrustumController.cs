0b1f0c32fa447c7771d1ee92e0b966060ee6b616
Ershany <bradyjessup@hotmail.com>
2018-03-28 01:19:29 -0400

    Added initial code in preperation for Fog of War. I will be using offscreen rendering (using orthographic camera) to make a masking texture for the Fog of War implementation

---
+++ 3D-RTS/Assets/Scripts/Camera/FrustumController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrustumController : MonoBehaviour {
+
+    private List<GameObject> playerObjectsInView;
+
+    void Awake()
+    {
+        playerObjectsInView = new List<GameObject>();    
+    }
+
+    public void AddObjectInView(GameObject gameObj)
+    {
+        playerObjectsInView.Add(gameObj);
+    }
+
+    public bool RemoveObjectInView(GameObject gameObj)
+    {
+        return playerObjectsInView.Remove(gameObj);
+    }
+}
