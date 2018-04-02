0b4e40af88bc7be191c958a1948830e5a2c52161
julian curran <julian_curran@hotmail.com>
2018-04-02 04:16:39 -0400

    forgot to add RandomEncounterController.cs last commit

--- 3D-RTS/Assets/Scripts/Environment/RandomEncounterController.cs
+++ 3D-RTS/Assets/Scripts/Environment/RandomEncounterController.cs
@@ -6,64 +6,117 @@ using UnityEngine;
 public class EnvironmentUnits
 {
     //store prefabs for static units here
-    public static  List<GameObject> staticUnits;
+    public static List<GameObject> neutralUnits;
 }
 
 public class RandomEncounterController : MonoBehaviour
 {
-    public StaticUnitController staticUnit;
+    public List<List<NeutralUnitController>> neutralUnits;
+    public GameController gameCon;
+    public PlayerController playCon;
     public bool battleEncountered;
+    public List<Group> battleGroups;
 
-	// Use this for initialization
-	void Start ()
+    public List<Group> objectsToIgnore;
+
+    // Use this for initialization
+    void Start()
     {
+        neutralUnits = new List<List<NeutralUnitController>>();
         battleEncountered = false;
-	}
-	
-	//LateUpdate just in case
-	void LateUpdate ()
-    {
-        //atm what i am gonna do is check it in update
-        if (staticUnit == null)
-        {
-            battleEncountered = false;
-            return;
-        }
-
-        //check if it is not in battle if it is not destroy the prefab
-        //if the static unit won the battle remove it
-        if (!staticUnit.unit.IsInBattle)
-        {
-            battleEncountered = false;
-            Destroy(staticUnit.unit.GetGameObject());
-        }
+        gameCon = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        playCon = GameObject.FindGameObjectWithTag("PlayerController").GetComponent<PlayerController>();
+        objectsToIgnore = new List<Group>();
     }
 
-    //on collision check for random encounter
-    void OnTriggerEnter(Collider other)
+    //LateUpdate just in case
+    public void UpdateRandomBattleController()
     {
-        if (battleEncountered) return;
+        if (battleGroups.Count < 1)
+            battleEncountered = false;
 
-        int random = Random.Range(0, 7);
-
-        //12.5% chance of random encounter (I think)
-        if (random > 1)
+        for (int i = 0; i < playCon.groups.Count; i++)
         {
-            FoeEncounter(other.transform);
-            Debug.Log("u have encountered an ememy unit");
-            battleEncountered = true;
+            if (objectsToIgnore.Contains(playCon.groups[i]))
+            {
+                continue;
+            }
+            for (int j = 0; j < playCon.groups[i].GetUnits().Count; j++)
+            {
+                int random = Random.Range(0, 7);
+
+                if (random > 1)
+                {
+                    if (gameObject.GetComponent<Collider>().bounds.Contains(playCon.groups[i].GetUnits()[j].GetTransform().position))
+                    {
+                        Debug.Log("Foe encountered");
+                        FoeEncounter(playCon.groups[i].GetUnits()[j].GetTransform());
+                        battleEncountered = true;
+                        objectsToIgnore.Add(playCon.groups[i]);
+                        battleGroups.Add(playCon.groups[i]);
+                        j = 4;
+                    }
+
+                }
+            }
         }
-        else { Debug.Log("u are fine u have survived this encounter"); }
+
+
+
     }
+
+    ////on collision check for random encounter
+    //void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("u have encountered an ememy unit");
+    //    for (int i = 0; i < objectsToIgnore.Count; i++)
+    //    {
+    //        if(other.gameObject == objectsToIgnore[i].gameObject)
+    //        {
+    //            Debug.Log("worked");
+    //            return;
+    //        }
+    //    }
+    //    int random = Random.Range(0, 7);
+
+    //    //12.5% chance of random encounter (I think)
+    //    if (random > 1)
+    //    {
+    //        Debug.Log("u have encountered an ememy unit");
+    //        FoeEncounter(other.transform);
+    //        battleEncountered = true;
+    //        objectsToIgnore.AddRange(other.transform.parent.GetComponentsInChildren<Transform>());
+    //    }
+    //}
 
     //setup a foe encounter
     void FoeEncounter(Transform trans)
     {
+        //Use to control number of enemies in a random encounter
+        int enemyCount = Random.Range(1, 2);
+        Debug.Log("enemyCount: " + enemyCount);
         //instantiate a random static unit
-        int index = Random.Range(0 , EnvironmentUnits.staticUnits.Count - 1);
-        GameObject unit = Instantiate(EnvironmentUnits.staticUnits[index], trans.position, Quaternion.identity);
+        List<NeutralUnitController> newBattleGroup = new List<NeutralUnitController>();
+        for (int i = 0; i < enemyCount; i++)
+        {
+            int index = Random.Range(0, EnvironmentUnits.neutralUnits.Count - 1);
+            // unit.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            newBattleGroup.Add(Instantiate(EnvironmentUnits.neutralUnits[0], trans.position, Quaternion.identity).GetComponent<NeutralUnitController>());
 
-        staticUnit = unit.GetComponent<StaticUnitController>();
-        Debug.Log(staticUnit.name);
+            Debug.Log(newBattleGroup[i].name);
+        }
+        neutralUnits.Add(newBattleGroup);
+    }
+
+    public List<DynamicUnit> GetUnits(int j)
+    {
+        List<DynamicUnit> temp = new List<DynamicUnit>();
+
+        for (int i = 0; i < neutralUnits[j].Count; i++)
+        {
+            temp.Add(neutralUnits[j][i].GetUnit());
+        }
+
+        return temp;
     }
 }
